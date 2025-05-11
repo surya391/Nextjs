840478c794bdeed50604f0f71f218d5fb40c3675
demo <suryamgowda93@gmail.com>
2025-05-11 21:28:58 +0530

    done with the not-found

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default function ProductReviews({
     params,
 }: {
     params: { productId: string; reviewId: string }
 }) {
     const { productId, reviewId } = params;
+    if(parseInt(reviewId) > 1000){
+        notFound()
+    }
     return (
         <>
             <h1>Review {reviewId} for product {productId}</h1>
